a76b92839032a1653053961798117b0e0ab1942b
mostafizz fuad <mostafizzfuad@gmail.com>
2023-02-20 23:53:15 +0600

    update code

--- evenOdd.php
+++ evenOdd.php
@@ -1,15 +1,4 @@
 <?php
 
-function evenOdd($n) {
-	if ($n % 2 == 0) {
-		return $n . " is an even number";
-	} 
-	else {
-		return $n . " is an odd number";
-	}
-}
-
-$checkingValue = 12;
-$result = evenOdd($checkingValue);
-
-echo $result;
+$fruit = array("apple","banana","cherry");
+echo implode(",",$fruit);
